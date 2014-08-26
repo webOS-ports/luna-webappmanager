d9136ed3b25659b907a05807f304625118b41da3
Simon Busch <morphis@gravedo.de>
2014-08-26 22:48:35 +0200

    Adjust to new location of system UI application
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/webapplicationwindow.cpp
+++ src/webapplicationwindow.cpp
@@ -384,7 +384,7 @@ QString WebApplicationWindow::getIdentifierForFrame(const QString& id, const QSt
 {
     QString identifier = mApplication->identifier();
 
-    if (url.startsWith("file:///usr/lib/luna/system/luna-systemui/"))
+    if (url.startsWith("file:///usr/palm/applications/com.palm.systemui"))
         identifier = QString("com.palm.systemui %1").arg(mApplication->processId());
 
     qDebug() << __PRETTY_FUNCTION__ << "Decided identifier for frame" << id << "is" << identifier;
