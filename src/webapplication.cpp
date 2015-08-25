2c7ab98aab2f9123cd05638793fea7714ea85827
Herrie <Github.com@herrie.org>
2015-08-25 07:28:46 +0200

    More logging for height

--- src/webapplication.cpp
+++ src/webapplication.cpp
@@ -217,8 +217,10 @@ void WebApplication::createWindow(QWebNewPageRequest *request)
         QVariant::Type type = windowFeatures["height"].type();
         if (type == QVariant::Int)
             height = windowFeatures["height"].toInt();
+            qDebug() << __PRETTY_FUNCTION__ << "height is in int value: " << height;
         else if (type == QVariant::Double)
             height = static_cast<int>(windowFeatures["height"].toDouble());
+            qDebug() << __PRETTY_FUNCTION__ << "height is in double value: " << height;
 
         if (windowMetrics == "units") {
             qDebug() << __PRETTY_FUNCTION__ << "windowMetrics == \"units\" Settings::LunaSettings()->gridUnit: " << Settings::LunaSettings()->gridUnit;
